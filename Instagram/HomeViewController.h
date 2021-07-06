71fea4f9a489a51018a7f969af69e8959b087a48
Santy Mendoza <55068232+santymendoza@users.noreply.github.com>
2021-07-06 14:18:03 -0700

    added log in and sign up features

---
+++ Instagram/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Instagram
+//
+//  Created by Santy Mendoza on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram/LoginViewController.m
@@ -0,0 +1,68 @@
+//
+//  LoginViewController.m
+//  Instagram
+//
+//  Created by Santy Mendoza on 7/6/21.
+//
+
+#import "LoginViewController.h"
+#import <Parse/Parse.h>
+
+@interface LoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameText;
+@property (weak, nonatomic) IBOutlet UITextField *passwordText;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)loginPressed:(id)sender {
+    NSString *username = self.userNameText.text;
+    NSString *password = self.passwordText.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"loggedInSegue" sender:nil];
+            // display view controller that needs to shown after successful login
+        }
+    }];
+}
+- (IBAction)signupPressed:(id)sender {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.userNameText.text;
+    newUser.password = self.passwordText.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            [self performSegueWithIdentifier:@"loggedInSegue" sender:nil];
+            
+            // manually segue to logged in view
+        }
+    }];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
